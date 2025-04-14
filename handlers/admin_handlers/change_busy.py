191ed6bbae502c9ab009bafb4618a6ec31eb95f6
babwalker <babwalkerr@gmail.com>
2025-04-14 19:42:54 +0400

    change_id_to_phone

--- handlers/admin_handlers/change_busy.py
+++ handlers/admin_handlers/change_busy.py
@@ -1,21 +1,21 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from database.get_to_db import get_taxi
+from database.get_to_db import get_taxi, get_taxi_by_phone
 from loader import bot, dp
 from states.admin_states import ChangeBusy
 
 @dp.callback_query_handler(lambda c: c.data == "busy_off", state="*")
 async def process_busy_off(callback_query: types.CallbackQuery, state: FSMContext):
     await state.finish()
-    await bot.send_message(callback_query.from_user.id, "Введите айди таксиста, у которого хотите снять статус is_busy")
+    await bot.send_message(callback_query.from_user.id, "Введите номер телефона таксиста, у которого хотите снять статус is_busy")
     await ChangeBusy.GET_USER_ID.set()
     await callback_query.answer()
 
 @dp.message_handler(state=ChangeBusy.GET_USER_ID)
 async def process_change_busy(message: types.Message, state: FSMContext):
-    user_id = message.text
-    taxi = await get_taxi(user_id)
+    phone = message.text
+    taxi = get_taxi_by_phone(phone)
     if taxi:
         taxi.is_busy = False
         taxi.save()
--- handlers/admin_handlers/change_rating.py
+++ handlers/admin_handlers/change_rating.py
@@ -2,30 +2,28 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 
 from database.database import Taxi, User
-from database.get_to_db import get_taxi, get_user
+from database.get_to_db import get_taxi_by_phone, get_user_by_phone
 from loader import bot, dp
 from states.admin_states import ChangeRating
 
 @dp.callback_query_handler(lambda c: c.data == 'change_rating', state="*")
 async def process_user_id(callback_query: types.CallbackQuery, state: FSMContext):
-    await bot.send_message(callback_query.from_user.id, f"Введите айди пользователя")
+    await bot.send_message(callback_query.from_user.id, f"Введите номер телефона пользователя")
     await state.set_state(ChangeRating.GET_USER_ID)
     await callback_query.answer()
 
 @dp.message_handler(state=ChangeRating.GET_USER_ID)
 async def process_change_rating(message: types.Message, state: FSMContext):
-    user_id = message.text
-    taxi = await get_taxi(user_id)
-    user = await get_user(user_id)
-    print(taxi)
+    phone = message.text
+    await state.update_data(phone=phone)
+    taxi = get_taxi_by_phone(phone)
+    user = get_user_by_phone(phone)
     if taxi:
         await bot.send_message(message.from_user.id, f"Введите новый рейтинг для таксиста")
         await state.set_state(ChangeRating.CHANGE_TAXI_RATING)
-        await state.update_data(taxi_id=user_id)
     elif user:
         await bot.send_message(message.from_user.id, f"Введите новый рейтинг для пользователя")
         await state.set_state(ChangeRating.CHANGE_USER_RATING)
-        await state.update_data(user_id=user_id)
     else:
         await bot.send_message(message.from_user.id, f"⚠️ Пользователь не найден в базе данных!")
         await state.finish()
@@ -34,7 +32,7 @@ async def process_change_rating(message: types.Message, state: FSMContext):
 async def process_change_taxi_rating(message: types.Message, state: FSMContext):
     new_rating = message.text
     data = await state.get_data()
-    taxi = Taxi.get(Taxi.user_id == data.get("taxi_id"))
+    taxi = Taxi.get(Taxi.phone == data.get("phone"))
     taxi.rating = new_rating
     taxi.save()
     await bot.send_message(message.from_user.id, f"✅ Рейтинг успешно изменен на <b>{new_rating}</b>", parse_mode='html')
@@ -44,7 +42,7 @@ async def process_change_taxi_rating(message: types.Message, state: FSMContext):
 async def process_change_user_rating(message: types.Message, state: FSMContext):
     new_rating = message.text
     data = await state.get_data()
-    user = User.get(User.user_id == data.get("user_id"))
+    user = User.get(User.phone == data.get("phone"))
     user.rating = new_rating
     user.save()
     await bot.send_message(message.from_user.id, f"✅ Рейтинг успешно изменен на <b>{new_rating}</b>", parse_mode='html')
